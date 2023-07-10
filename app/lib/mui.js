e7c3fe452e6e215c49347dab7b081a68d4432824
usam6049 <usam6049@uni.sydney.edu.au>
2023-07-10 11:18:46 +1000

    Finish routing

--- app/lib/mui.js
+++
@@ -1,8 +0,0 @@
-"use client";
-
-export SearchIcon from '@mui/icons-material/Search';
-export { Button, Divider, Stack, Typography } from '@mui/material';
-export { Typography, Rating, Button, Stack, Box, Link } from '@mui/material';
-export RectangleIcon from '@mui/icons-material/Rectangle';
-export ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-
--- app/page.js
+++ app/page.js
@@ -1,32 +1,7 @@
-import Image from 'next/image'
-import './globals.css'
-import Header from './Header';
-import UnitDescription from './UnitDescription';
-import NavigationBar from './NavigationBar';
-import Reviews from './Reviews';
-import VerticalLinearStepper from './steps';
-import UnitOutline from './UnitOutline';
-// import Timetable from './Timetable';
-// import Tree from './Tree';
 
-export default function Page() {
-  return (
-    <div className="App">
-    <Header/>
-      <div className='HomePageWrapper'>
-        <div className='leftNavigationWrapper'>
-          <NavigationBar />
-        </div>
-        <div className='centerContentWrapper'>
-          <UnitDescription />
-          <UnitOutline />
-          <Reviews />
-        </div>
-        <div className='rightNavigationWrapper'>
-        <VerticalLinearStepper />
-        </div>
-      </div>
-    </div>
-  )
+export default function page() {
+    return (
+        <></>
+    )
+    
 }
-
