319325d760f89a07d26907e7e61a766b12c09df2
usam6049 <usam6049@uni.sydney.edu.au>
2023-07-20 21:19:33 +1000

    Add signin and signup pages

--- src/app/(mainPages)/signin/page.js
+++ src/app/(mainPages)/signin/page.js
@@ -1,6 +1,6 @@
 'use client'
 import React from "react";
-import signIn from "@/firebase/auth/signin";
+import signIn from "src/firebase/auth/signin";
 import { useRouter } from 'next/navigation'
 
 function Page() {
--- src/app/(mainPages)/signup/page.js
+++ src/app/(mainPages)/signup/page.js
@@ -1,6 +1,6 @@
 'use client'
 import React from "react";
-import signUp from "@/firebase/auth/signup";
+import signUp from "src/firebase/auth/signup";
 import { useRouter } from 'next/navigation'
 
 function Page() {
