6bff9fe53d50162d9592b4b432732ce118623e56
Jennifer Tan <jtan8485@uni.sydney.edu.au>
2023-07-20 11:03:01 +1000

    Finished home page (not responsive)

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { Container, Stack, Card, CardContent, Typography, Button, Box } from '@mui/material';
+import Image from 'next/image';
 import { BsFillCalendarEventFill, BsFillCalendar2Fill } from 'react-icons/bs';
 import { AiFillRead } from 'react-icons/ai';
 
@@ -13,16 +14,17 @@ export default function page() {
                 <Box sx={{ display: "flex", justifyContent: "center"}}>
                     <Button variant="contained" sx={{ marginTop: 5, marginBottom: 10, width: 180, backgroundColor: "#DD432B" }}>Sign In</Button>
                 </Box>
-                {/* <Image src="/Amigos_Standing.png" alt="Amigos Standing" width={244} height={354}/> */}
+                <Image src="/Amigos_Standing.png" alt="Amigos Standing" id="standing" width={345} height={500}/>
+                <Image src="/Amigos_Planner.png" alt="Amigos Planner" id="planner" width={220} height={210}/>
             </div>
             <div className="bottomContentWrapper">
-                <Stack direction="row" spacing={10}>
-                    <Container width={400}>
-                        <Typography sx={{ color: 'white', fontSize: 40, fontWeight: 700, width: 400 }}>Features</Typography>
-                        <Typography sx={{ color: 'white', fontSize: 20, width: 400 }}>Dummy text</Typography>
-                    </Container>
+                <Stack direction="row" spacing={10} alignItems="center">
+                    <Stack direction="column">
+                        <Typography sx={{ color: 'white', fontSize: 40, fontWeight: 700, width: 600 }}>Features</Typography>
+                        <Typography sx={{ color: 'white', fontSize: 20, width: 600 }}>Dummy text</Typography>
+                    </Stack>
 
-                    <Card sx={{ maxWidth: 220, maxHeight: 230 }}>
+                    <Card sx={{ width: 250, height: 265, borderRadius: 10, display: "flex", alignItems: "center" }}>
                         <CardContent>
                             <Stack direction="column" alignItems="center">
                                 <BsFillCalendarEventFill size="40px"/>
@@ -32,7 +34,7 @@ export default function page() {
                         </CardContent>
                     </Card>
 
-                    <Card sx={{ maxWidth: 220, maxHeight: 230 }}>
+                    <Card sx={{ width: 250, height: 265, borderRadius: 10, display: "flex", alignItems: "center" }}>
                         <CardContent>
                             <Stack direction="column" alignItems="center">
                                 <BsFillCalendar2Fill size="40px"/>
@@ -42,7 +44,7 @@ export default function page() {
                         </CardContent>
                     </Card>
 
-                    <Card sx={{ maxWidth: 220, maxHeight: 230 }}>
+                    <Card sx={{ width: 250, height: 265, borderRadius: 10, display: "flex", alignItems: "center" }}>
                         <CardContent>
                             <Stack direction="column" alignItems="center">
                                 <AiFillRead size="50px"/>
